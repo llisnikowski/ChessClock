b318e0f0618e8f97632a898f57f54cedfec4e6a9
Lukasz Lisnikowski <l.lisnikowski@gmail.com>
2022-08-20 17:30:56 +0200

    Add equal operator to time class

--- Core/Time/timeHMS.cpp
+++ Core/Time/timeHMS.cpp
@@ -7,127 +7,20 @@
 
 #include "timeHMS.hpp"
 
-constexpr TimeHMS::TimeHMS()
-	:hour_{0}, minute_{0}, second_{0}
-{
-}
-
-constexpr TimeHMS::TimeHMS(int8_t hour, int8_t minute, int8_t secound)
-	:hour_{hour}, minute_{minute}, second_{secound}
-{
-	correctTime();
-}
-
-
-
-bool TimeHMS::addSeconds(int8_t s)
-{
-	second_ += s;
-	return correctTime();
-}
-
-bool TimeHMS::addMinutes(int8_t m)
-{
-	minute_ += m;
-	return correctTime();
-}
-
-bool TimeHMS::addHours(int8_t h)
-{
-	hour_ += h;
-	return correctTime();
-}
-
-void TimeHMS::setHour(int8_t h)
-{
-	hour_ = h;
-	correctTime();
-}
-
-void TimeHMS::setMinute(int8_t m)
-{
-	minute_ = m;
-	correctTime();
-}
-
-void TimeHMS::setSecond(int8_t s)
-{
-	second_ = s;
-	correctTime();
-}
-
-void TimeHMS::setTime(int8_t h, int8_t m,int8_t s)
-{
-	hour_ = h;
-	minute_ = m;
-	second_ = s;
-	correctTime();
-}
-
-
-bool TimeHMS::correctTime()
-{
-	while(second_ >= 60){
-		minute_ ++;
-		second_ -= 60;
-	}
-	while (second_ < 0){
-		minute_--;
-		second_ += 60;
-	}
-	while(minute_ >= 60){
-		hour_ ++;
-		minute_ -= 60;
-	}
-	while(minute_ < 0){
-		hour_--;
-		minute_ += 60;
-	}
-	if(hour_ >= 24){
-		hour_ = hour_%24;
-		return true;
-	}
-	else if(hour_ < 0){
-		hour_ = 23 + (hour_ + 1) % 24;
-		return true;
-	}
-	return false;
-}
-
-
-
-bool TimeHMS::operator+=(const TimeHMS & t)
-{
-	this->second_ += t.second_;
-	this->minute_ += t.minute_;
-	this->hour_ += t.hour_;
-	return correctTime();
-}
-
-bool TimeHMS::operator-=(const TimeHMS & t)
-{
-	this->second_ -= t.second_;
-	this->minute_ -= t.minute_;
-	this->hour_ -= t.hour_;
-	return correctTime();
-}
 
 
 
 
-TimeHMS operator+(TimeHMS & t1, TimeHMS & t2)
-{
-	TimeHMS t = t1;
-	t += t2;
-	return t;
-}
 
-TimeHMS operator-(TimeHMS & t1, TimeHMS & t2)
-{
-	TimeHMS t = t1;
-	t -= t2;
-	return t;
-}
+
+
+
+
+
+
+
+
+
 
 
 
--- Core/Time/timeHMS.hpp
+++ Core/Time/timeHMS.hpp
@@ -12,35 +12,162 @@
 
 class TimeHMS {
 public:
-	constexpr TimeHMS();
-	constexpr TimeHMS(int8_t hour, int8_t minute, int8_t second);
+	constexpr TimeHMS()
+		:hour_{0}, minute_{0}, second_{0}
+	{}
+	constexpr TimeHMS(int8_t hour, int8_t minute, int8_t second)
+		:hour_{hour}, minute_{minute}, second_{second}
+	{
+		correctTime();
+	}
+
 	~TimeHMS() = default;
 
-	bool addSeconds(int8_t s);
-	bool addMinutes(int8_t m);
-	bool addHours(int8_t h);
+	constexpr bool addSeconds(int8_t s);
+	constexpr bool addMinutes(int8_t m);
+	constexpr bool addHours(int8_t h);
 
-	void setHour(int8_t h);
-	void setMinute(int8_t m);
-	void setSecond(int8_t s);
-	void setTime(int8_t h, int8_t m,int8_t s);
+	constexpr void setHour(int8_t h);
+	constexpr void setMinute(int8_t m);
+	constexpr void setSecond(int8_t s);
+	constexpr void setTime(int8_t h, int8_t m,int8_t s);
 	constexpr int8_t getHour() const {return hour_;}
 	constexpr int8_t getMinute() const {return minute_;}
 	constexpr int8_t getSecond() const {return second_;}
 
-	bool operator+=(const TimeHMS & t);
-	bool operator-=(const TimeHMS & t);
+	constexpr bool operator+=(const TimeHMS & t);
+	constexpr bool operator-=(const TimeHMS & t);
+	constexpr bool operator==(const TimeHMS & t);
 
 private:
-	bool correctTime();
+	constexpr bool correctTime();
 
 	int8_t hour_;
 	int8_t minute_;
 	int8_t second_;
 };
 
-TimeHMS operator+(TimeHMS & t1, TimeHMS & t2);
-TimeHMS operator-(TimeHMS & t1, TimeHMS & t2);
+
+
+constexpr TimeHMS operator+(TimeHMS & t1, TimeHMS & t2);
+constexpr TimeHMS operator-(TimeHMS & t1, TimeHMS & t2);
+
+
+
+//------------------------------------------------------
+
+constexpr bool TimeHMS::addSeconds(int8_t s)
+{
+	second_ += s;
+	return correctTime();
+}
+
+constexpr bool TimeHMS::addMinutes(int8_t m)
+{
+	minute_ += m;
+	return correctTime();
+}
+
+constexpr bool TimeHMS::addHours(int8_t h)
+{
+	hour_ += h;
+	return correctTime();
+}
+
+constexpr void TimeHMS::setHour(int8_t h)
+{
+	hour_ = h;
+	correctTime();
+}
+
+constexpr void TimeHMS::setMinute(int8_t m)
+{
+	minute_ = m;
+	correctTime();
+}
+
+constexpr void TimeHMS::setSecond(int8_t s)
+{
+	second_ = s;
+	correctTime();
+}
+
+constexpr void TimeHMS::setTime(int8_t h, int8_t m,int8_t s)
+{
+	hour_ = h;
+	minute_ = m;
+	second_ = s;
+	correctTime();
+}
+
+
+constexpr bool TimeHMS::correctTime()
+{
+	while(second_ >= 60){
+		minute_ ++;
+		second_ -= 60;
+	}
+	while (second_ < 0){
+		minute_--;
+		second_ += 60;
+	}
+	while(minute_ >= 60){
+		hour_ ++;
+		minute_ -= 60;
+	}
+	while(minute_ < 0){
+		hour_--;
+		minute_ += 60;
+	}
+	if(hour_ >= 24){
+		hour_ = hour_%24;
+		return true;
+	}
+	else if(hour_ < 0){
+		hour_ = 23 + (hour_ + 1) % 24;
+		return true;
+	}
+	return false;
+}
+
+
+constexpr bool TimeHMS::operator+=(const TimeHMS & t)
+{
+	this->second_ += t.second_;
+	this->minute_ += t.minute_;
+	this->hour_ += t.hour_;
+	return correctTime();
+}
+
+constexpr bool TimeHMS::operator-=(const TimeHMS & t)
+{
+	this->second_ -= t.second_;
+	this->minute_ -= t.minute_;
+	this->hour_ -= t.hour_;
+	return correctTime();
+}
+
+constexpr bool TimeHMS::operator==(const TimeHMS & t)
+{
+	return hour_ == t.hour_ && minute_ == t.minute_ && second_ == t.second_;
+}
+
+//------------------------
+
+constexpr TimeHMS operator+(TimeHMS & t1, TimeHMS & t2)
+{
+	TimeHMS t = t1;
+	t += t2;
+	return t;
+}
+
+constexpr TimeHMS operator-(TimeHMS & t1, TimeHMS & t2)
+{
+	TimeHMS t = t1;
+	t -= t2;
+	return t;
+}
+
 
 
 #endif /* TIME_TIMEHMS_HPP_ */
